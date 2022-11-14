6ab4f6dc28074a8e9ed345101f8a156957005186
Can Kanbur <cankanbur1@gmail.com>
2022-11-14 20:48:15 +0300

    island view added

--- App.js
+++ App.js
@@ -1,55 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import { StyleSheet, View, useWindowDimensions } from 'react-native';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
   withTiming,
   Easing,
 } from 'react-native-reanimated';
-
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 const normalizeVector = vector => {
   const length = Math.sqrt(vector.x * vector.x + vector.y * vector.y);
   return { x: vector.x / length, y: vector.y / length };
 };
+const BALL_SIZE = 25;
 const FPS = 60;
-const DELTA = 1 / FPS;
-const SPEED = 30;
-const BALL_WIDTH = 25;
+const DELTA = 1000 / FPS;
+const SPEED = 14;
+
+const islandDimensions = { x: 150, y: 11, w: 127, h: 37 };
+
 const App = () => {
   const { width, height } = useWindowDimensions();
-
-  const targetPositionX = useSharedValue(200);
-  const targetPositionY = useSharedValue(200);
-
+  const targetPositionX = useSharedValue(100);
+  const targetPositionY = useSharedValue(100);
   const direction = useSharedValue(
     normalizeVector({ x: Math.random(), y: Math.random() })
   );
 
+  useEffect(() => {
+    setInterval(updatePosition, DELTA);
+    return () => clearInterval(updatePosition);
+  }, []);
   const updatePosition = () => {
-    let nextPos = nextPosition(direction.value);
-
-    if (nextPos.y < 0 || nextPos.y > height - BALL_WIDTH) {
-      const newDirection = { x: direction.value.x, y: -direction.value.y };
-      direction.value = newDirection;
-      nextPos = nextPosition(newDirection);
+    let nextPos = getPosition(direction.value);
+    if (nextPos.x < 0 || nextPos.x > width - BALL_SIZE) {
+      const newValue = { x: -direction.value.x, y: direction.value.y };
+      direction.value = newValue;
+      nextPos = getPosition(newValue);
     }
-    if (nextPos.x < 0 || nextPos.x > width - BALL_WIDTH) {
-      const newDirection = { x: -direction.value.x, y: direction.value.y };
-      direction.value = newDirection;
-      nextPos = nextPosition(newDirection);
+    if (nextPos.y < 0 || nextPos.y > height - BALL_SIZE) {
+      const newValue = { x: direction.value.x, y: -direction.value.y };
+      direction.value = newValue;
+      nextPos = getPosition(newValue);
+    }
+    if (
+      nextPos.x < islandDimensions.x + islandDimensions.w &&
+      nextPos.x + BALL_SIZE > islandDimensions.x &&
+      nextPos.y < islandDimensions.y + islandDimensions.h &&
+      nextPos.y + BALL_SIZE > islandDimensions.y
+    ) {
+      if (
+        targetPositionX.value < islandDimensions.x ||
+        targetPositionX.value > islandDimensions.x + islandDimensions.w
+      ) {
+        const newValue = { x: -direction.value.x, y: direction.value.y };
+        direction.value = newValue;
+        nextPos = getPosition(newValue);
+      } else {
+        const newValue = { x: direction.value.x, y: -direction.value.y };
+        direction.value = newValue;
+        nextPos = getPosition(newValue);
+      }
     }
 
     targetPositionX.value = withTiming(nextPos.x, {
-      duration: DELTA * 1000,
+      duration: DELTA,
       easing: Easing.linear,
     });
     targetPositionY.value = withTiming(nextPos.y, {
-      duration: DELTA * 1000,
+      duration: DELTA,
       easing: Easing.linear,
     });
   };
-  const nextPosition = direction => {
+
+  const getPosition = direction => {
     return {
       x: targetPositionX.value + direction.x * SPEED,
       y: targetPositionY.value + direction.y * SPEED,
@@ -58,18 +81,26 @@ const App = () => {
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
-      left: targetPositionX.value,
       top: targetPositionY.value,
+      left: targetPositionX.value,
     };
   });
-  useEffect(() => {
-    const interval = setInterval(updatePosition, 500);
-    return () => clearInterval(interval);
-  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Animated.View style={[styles.ball, animatedStyle]} />
+      <View
+        style={{
+          width: islandDimensions.w,
+          height: islandDimensions.h,
+          backgroundColor: 'black',
+          borderRadius: 20,
+          position: 'absolute',
+          top: islandDimensions.y,
+          left: islandDimensions.x,
+        }}
+      ></View>
     </View>
   );
 };
@@ -83,7 +114,7 @@ const styles = StyleSheet.create({
   },
   ball: {
     backgroundColor: 'black',
-    width: BALL_WIDTH,
+    width: BALL_SIZE,
     aspectRatio: 1,
     borderRadius: 25,
     position: 'absolute',
